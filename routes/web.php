fa58c7b226f7113b3e3d25941e0ffed50708a7e5
taku <ta_msy.4.11_35@icloud.com>
2020-11-22 20:20:09 +0900

    [Fix]未ログイン時のアクセス制限設定

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,22 @@
 
 Route::get('/', 'ProductionPlanOnChargeController@index')->name('production_plan_on_charge.index');
 
-Route::resource('product_item', 'ProductItemController', ['except' => ['show']]);
+Route::group(['middleware' => 'auth'], function() {
+  Route::resource('product_item', 'ProductItemController', ['except' => ['show']]);
 
-Route::resource('charge', 'ChargeController');
+  Route::resource('charge', 'ChargeController');
 
-Route::delete('charge_can_work/{product_item_id}/{charge_id}', 'ChargeCanWorkController@destroy')->name('charge_can_work.destroy');
-Route::resource('charge_can_work', 'ChargeCanWorkController', ['only' => ['store']]);
+  Route::delete('charge_can_work/{product_item_id}/{charge_id}', 'ChargeCanWorkController@destroy')->name('charge_can_work.destroy');
+  Route::resource('charge_can_work', 'ChargeCanWorkController', ['only' => ['store']]);
 
-Route::get('production_plan_on_charge/create/{product_item_id}', 'ProductionPlanOnChargeController@create')->name('production_plan_on_charge.create');
-Route::resource('production_plan_on_charge', 'ProductionPlanOnChargeController', ['only' => ['index','store']]);
+  Route::get('production_plan_on_charge/create/{product_item_id}', 'ProductionPlanOnChargeController@create')->name('production_plan_on_charge.create');
+  Route::resource('production_plan_on_charge', 'ProductionPlanOnChargeController', ['only' => ['index','store']]);
 
-Route::get('production_act_on_charge/create/{product_item_id}', 'ProductionActOnChargeController@create')->name('production_act_on_charge.create');
-Route::resource('production_act_on_charge', 'ProductionActOnChargeController', ['only' => ['index','store']]);
-
-
+  Route::get('production_act_on_charge/create/{product_item_id}', 'ProductionActOnChargeController@create')->name('production_act_on_charge.create');
+  Route::resource('production_act_on_charge', 'ProductionActOnChargeController', ['only' => ['index','store']]);
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
