6b916bda7c27a9343c438ce7cc771c7d2b005826
Kazumasa Tanaka <k.tanaka@rikido.net>
2020-11-25 22:46:00 +0900

    開始シェルを変更

--- docker/start.sh
+++ docker/start.sh
@@ -7,13 +7,12 @@ docker-compose up -d
 sleep 10
 
 if [ ${PS_CNT} -lt 1 ]; then
-    container_name=lamp_web
-    docker exec ${container_name} ln -s /docker_initial_files/etc/httpd/conf.d/httpd.conf /etc/httpd/conf.d/httpd.conf
-    docker exec ${container_name} mysql_install_db --datadir=/var/lib/mysql --user=mysql
-    docker exec ${container_name} ln -s /docker_initial_files/etc/my.cnf.d/mysql.server.ext.cnf /etc/my.cnf.d/mysql.server.ext.cnf
-    docker exec ${container_name} ln -s /docker_initial_files/etc/my.cnf.d/mysql.client.ext.cnf /etc/my.cnf.d/mysql.client.ext.cnf
-    docker exec ${container_name} ln -s /docker_initial_files/etc/php.d/xdebug.ini /etc/php.d/xdebug.ini
+    docker exec ${CONTAINER_NAME} ln -s /docker_initial_files/etc/httpd/conf.d/httpd.conf /etc/httpd/conf.d/httpd.conf
+    docker exec ${CONTAINER_NAME} mysql_install_db --datadir=/var/lib/mysql --user=mysql
+    docker exec ${CONTAINER_NAME} ln -s /docker_initial_files/etc/my.cnf.d/mysql.server.ext.cnf /etc/my.cnf.d/mysql.server.ext.cnf
+    docker exec ${CONTAINER_NAME} ln -s /docker_initial_files/etc/my.cnf.d/mysql.client.ext.cnf /etc/my.cnf.d/mysql.client.ext.cnf
+    docker exec ${CONTAINER_NAME} ln -s /docker_initial_files/etc/php.d/xdebug.ini /etc/php.d/xdebug.ini
 fi
  
-docker exec ${container_name} chmod 777 /usr/local/bin/web/startup.sh
-docker exec ${container_name} /usr/local/bin/web/startup.sh
+docker exec ${CONTAINER_NAME} chmod 777 /usr/local/bin/web/startup.sh
+docker exec ${CONTAINER_NAME} /usr/local/bin/web/startup.sh
